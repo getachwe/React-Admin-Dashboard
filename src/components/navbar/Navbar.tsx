f2d0eed113f7aef552e8bc16690a2274c132ee7d
getachwe <getachwe391@gmail.com>
2023-09-27 01:04:14 +0300

    add navbar and menu

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,27 @@
+import "./navbar.scss";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="" />
+        <span>lamadmin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notifications">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="" alt="" />
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/components/navbar/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <span>lamadmin</span>
+      <span>gwtachwe</span>
+    </div>
+  );
+}
+
+export default Footer;
